6892d3c78087c8128ad7aaad5e7683b50940912c
Monica Zamora Z <monicazamoraz@gmail.com>
2017-12-30 13:15:24 -0800

    Check redundance at all levels

--- Soundings/Get_sounding_Var.m
+++ Soundings/Get_sounding_Var.m
@@ -54,13 +54,12 @@ for i=1:length(ListofVar)
     varargout{i}=Var;
 end                 
 
-%% Check that mandatory levels aren't duplicating temperature values in the BL
+%% Check that there aren't duplicated values in the BL
 p=varargout{1}; tp=varargout{3};
 
 idelete=[];
-for pman=[925 850 700 500 500 400 300 250 200 150 100] %go through mandatory levels (except for the surface)
-    istar=find(p==pman); 
-    if (tp(istar)==tp(istar+1))|(tp(istar)==tp(istar-1)) %the temp is the same up or down
+for istar=1:length(tp)-1 %go through all levels
+    if (tp(istar)==tp(istar+1)) %the temp is the same up or down
          idelete=[idelete, istar']; %add this index to the ones to be deleted
     end
 end

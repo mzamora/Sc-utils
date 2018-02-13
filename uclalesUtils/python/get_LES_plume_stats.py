b033dcd3b51210c0afd9710467dc8d20cf4d507f
Elynn Wu <elynn510@gmail.com>
2018-02-13 13:06:42 -0800

    adding plotting function

--- uclalesUtils/python/get_LES_plume_stats.py
+++ uclalesUtils/python/get_LES_plume_stats.py
@@ -20,8 +20,8 @@ run_stitched_file = 'rf01_stitched.nc'
 run_time_res = 60. #LES time resolution
 run_startTime = 10860. #LES start time
 run_endTime = 14400. #LES end time
-z_scale = 'zi3_bar'
-output_dir = '/home/elynn/Documents/uclales/dycomsrf01_br02/RF01_hour34_1min/plume_output/zi3_bar_5percent_plume/'
+z_scale = 'zi1_bar'
+output_dir = '/home/elynn/Documents/uclales/dycomsrf01_br02/RF01_hour34_1min/plume_output/zi1_bar_5percent_plume/'
 '''End configuration'''
 
 '''Read time statistic file and stitched LES file'''
@@ -34,6 +34,8 @@ w = les_ncfile['w']
 ql = les_ncfile['l']
 t = les_ncfile['t']
 z = les_ncfile['zm'][:]
+
+'''3 loops - time, variables, vertical slice'''
 for t_index in range(60):
     flag = True #first time in each t_index
     for vc in range(len(plume_vars)): #loop over plume profiles
@@ -73,3 +75,41 @@ for t_index in range(60):
             output['updraft_'+plume_vars[vc]] = vertical_var_ud
             output['downdraft_'+plume_vars[vc]] = vertical_var
     output.to_csv(output_dir+'Vertical_profile_at_time_'+str(t_index+1)+'.csv')
+
+'''plotting function'''
+def plot_hourly_avg_vert_profile(plume_vars,output_dir):
+    xlabels = [r'$\theta_l$ [K]',r'$q_T$ [g/kg]',r'$q_l$ [g/kg]',r'$w$ [m/s]']
+    xlimits = [[289.1,289.5],[8.5,9.5],[-0.01,0.7],[-2.0,2.0]]
+    flag = True
+    for t_index in range(1,61):
+        current = pd.read_csv(output_dir+'Vertical_profile_at_time_'+str(t_index)+'.csv',index_col=0) 
+        if flag:
+            output = current
+            flag = False
+        else:
+            output = pd.concat([output,current],axis=1)
+    plt.figure(figsize=(15,5))
+    for i in range(len(plume_vars)):
+        plume_var = plume_vars[i]
+        if (plume_var == 'q') or (plume_var == 'l'):
+            domain = output['domain_'+plume_var].mean(axis=1)*1000.
+            updraft = output['updraft_'+plume_var].mean(axis=1)*1000.
+            downdraft = output['downdraft_'+plume_var].mean(axis=1)*1000.  
+        else:
+            domain = output['domain_'+plume_var].mean(axis=1)
+            updraft = output['updraft_'+plume_var].mean(axis=1)
+            downdraft = output['downdraft_'+plume_var].mean(axis=1)     
+        plt.subplot(1,4,i+1)
+        plt.plot(domain.iloc[:],domain.index[:],color='k',label='domain')
+        plt.plot(updraft.iloc[:],domain.index[:],color='r',label='updraft')
+        plt.plot(downdraft.iloc[:],domain.index[:],color='b',label='downdraft')
+        plt.xlabel(xlabels[i])
+        plt.xlim(xlimits[i])
+        plt.ylabel(r'$z/z_*$ [-]')
+        if i==0:
+            plt.legend(loc=0) 
+    suptitle = plt.suptitle('Hour3-4 average', y=1.05)
+    plt.tight_layout()
+    plt.savefig(output_dir+'Plume_vars_hour_34_avg.png',dpi=200,bbox_inches='tight',bbox_extra_artists=[suptitle])
+
+plot_hourly_avg_vert_profile(plume_vars,output_dir)
